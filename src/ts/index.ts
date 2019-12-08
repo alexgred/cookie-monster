5ab38520acaaef6a1d7fe8e171a68810d509f63c
Alex Dolzhenkov <alexgred.dev@gmail.com>
2019-12-08 18:59:55 +0300

    Cookiemonster code

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,45 @@
-import hello from './test';
+interface IOptions {
+  path?: string;
+  domain?: string;
+  'max-age'?: number;
+  expire?: string;
+  secure?: boolean;
+  samesite?: string;
+}
 
-console.log(hello);
+export default class CookieMonster {
+
+  static get(name: string): string {
+    const value: string = '; ' + document.cookie;
+    const parts: string[] = value.split("; " + name + "=");
+
+    if (parts.length == 2) {
+      return parts.pop().split(";").shift();
+    }
+  }
+
+  static set(name: string, value: string, options?: IOptions): void {
+    options = {
+      path: '/',
+      ...options
+    };
+
+    let cookie: string = name + '=' + value;
+
+    Object.keys(options).forEach((key: keyof IOptions) => {
+      cookie += '; ' + key;
+
+      if(options[key] !== true) {
+        cookie += '=' + options[key];
+      }
+    });
+
+    document.cookie = cookie;
+  }
+
+  static delete(name: string): void {
+    this.set(name, '', {
+      'max-age': -1
+    });
+  }
+}
